bc1b56671ab8320a6143626fab43c71c3742f4a3
prabhavsetia <prabhav.setia21@gmail.com>
2024-05-11 01:18:39 +0530

    last commit

--- index.js
+++ index.js
@@ -3,34 +3,19 @@ function updateMap() {
         .then(response => response.json())
         .then(data => {
             data.forEach(country => {
-                 lat = country.countryInfo.lat;
-                 long = country.countryInfo.long;
+                lat = country.countryInfo.lat;
+                long = country.countryInfo.long;
 
                 cases = country.cases;
-                if (cases <50000){
+                if (cases < 50000) {
                     color = "yellow"
                 }
-                else if (cases < 3000000 && cases>50000) {
+                else if (cases < 3000000 && cases > 50000) {
                     color = "orange"
                 }
-                else if (cases>3000000){
-                    color= "red"
+                else if (cases > 3000000) {
+                    color = "red"
                 }
-
-                map.addLayer({
-                    'id': 'urban-areas-fill',
-                    'type': 'fill',
-                    // This property allows you to identify which `slot` in
-                    // the Mapbox Standard your new layer should be placed in (`bottom`, `middle`, `top`).
-                    'slot': 'middle',
-                    'source': 'urban-areas',
-                    'layout': {},
-                    'paint': {
-                        'fill-color': color,
-                        'fill-opacity': 0.4
-                    }
-                });
-        
 
                 new mapboxgl.Marker({
                     color: color
